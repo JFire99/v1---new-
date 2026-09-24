73b6317f01d19eec5b9690a62f434315af225f33
JFire99 <jfire99@hotmail.co.uk>
2026-09-24 16:50:50 +0100

    Allow opening price to be seeded after stock creation

--- src/scanner-engine.ts
+++ src/scanner-engine.ts
@@ -36,7 +36,7 @@ interface StockInternal {
   relativeVolume: number | null;
   volumeAcceleration: number | null;
   minuteVolume: number | null;
-  dayOpen: number | null;
+  dayOpen?: number | null;
   dayHigh: number;
   distanceFromHigh: number | null;
   score: number;
